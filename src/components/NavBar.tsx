ce92c52e51aec45ee4df630591ccb6cda68b7dcd
George Utsin <georgeutsin@gmail.com>
2020-04-05 16:31:42 -0400

    fix footer and refactor css (#6)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,21 +2,7 @@ import React from "react"
 import Logo from "./Logo";
 import './navbar.scss'
 
-// FontAwesome bars icon: https://fontawesome.com/icons/bars
-const collapseSVG = <svg
-    aria-hidden="true"
-    focusable="false"
-    role="img"
-    viewBox="0 0 448 512"
-    className="collapseIcon">
-    <path
-        fill="currentColor"
-        d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 
-        7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 
-        16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 
-        0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
-    </path>
-</svg>;
+const collapseSVG = <svg viewBox="0 0 448 512" className="collapse icon"><path></path></svg>;
 
 class NavBar extends React.Component<any, any> {
     constructor(props: any) {
