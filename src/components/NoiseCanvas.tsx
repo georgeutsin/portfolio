92c16f115091574f0e5439d915ee761be88477ec
George Utsin <georgeutsin@gmail.com>
2020-04-05 16:06:11 -0400

    add landing page main view

---
+++ src/components/NoiseCanvas.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+import Noise from "../utils/Noise";
+
+class NoiseCanvas extends React.Component<any, any> {
+    private ctx: CanvasRenderingContext2D | null;
+    private canvasRef: React.RefObject<HTMLCanvasElement>;
+    private noise: Noise;
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            canvasHeight: window.innerHeight,
+            canvasWidth: document.body.clientWidth,
+        };
+
+        this.ctx = null;
+        this.canvasRef = React.createRef<HTMLCanvasElement>();
+        this.noise = new Noise();
+
+        window.addEventListener("resize", this.windowResizeListener.bind(this));
+        window.addEventListener("orientationchange", this.windowResizeListener.bind(this));
+    }
+
+    public windowResizeListener() {
+        this.setState({
+            canvasHeight: window.innerHeight,
+            canvasWidth: document.body.clientWidth,
+        }, () => {
+            this.noise.updateCanvasDims();
+        });
+
+    }
+
+    public componentDidMount() {
+        if (this.canvasRef.current) { // Shouldn't be null, since its declared in the constructor.
+            this.ctx = this.canvasRef.current.getContext("2d");
+            this.noise.setCtx(this.ctx);
+            this.windowResizeListener(); // to fix canvas width bug
+        }
+    }
+
+    public componentWillUnmount() {
+        window.removeEventListener("resize", this.windowResizeListener);
+        window.removeEventListener("orientationchange", this.windowResizeListener);
+    }
+
+    render() {
+        return <canvas
+            className="noiseCanvas"
+            ref={this.canvasRef}
+            width={this.state.canvasWidth}
+            height={this.state.canvasHeight}>
+            Your browser does not support canvas element.
+        </canvas>;
+    }
+}
+
+export default NoiseCanvas;
