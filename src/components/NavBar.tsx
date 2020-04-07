96aac5eb21e87306270f3fcfae663d068ec6feba
George Utsin <georgeutsin@gmail.com>
2020-04-07 16:47:58 -0400

    fix bugs in safari (#12)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,12 @@ import React from "react"
 import Logo from "./Logo";
 import './navbar.scss'
 
-const collapseSVG = <svg viewBox="0 0 448 512" className="collapse icon"><path></path></svg>;
+const collapseSVG = <svg viewBox="0 0 448 512" className="collapse icon">
+    <path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837
+    7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0
+    8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16
+    16v40c0 8.837 7.163 16 16 16z" fill="currenColor"></path>
+</svg>;
 
 class NavBar extends React.Component<any, any> {
     constructor(props: any) {
@@ -38,12 +43,11 @@ class NavBar extends React.Component<any, any> {
                     </a>
 
                     <button className="collapseMenu" onClick={this.toggleMenu}>{collapseSVG}</button>
-
                     <div className={this.state.menuOpen ? "navItems open" : "navItems"} id="navItems">
                         <a href="/projects"><div className="link">Projects</div></a>
                         <a href="/photos"><div className="link">Photos</div></a>
                     </div>
-                    <div style={{ clear: "both" }}></div>
+
                 </div>
             </div>
         </div>;
